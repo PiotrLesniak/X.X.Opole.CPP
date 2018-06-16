ad42297d90c0d2c01c7d3c93d6537e2950302fb7
Piotr Leśniak <plesniak@future-processing.com>
2018-06-16 09:19:05 +0200

    egzamin - puste

---
+++ Egzamin/Egzamin/Egzamin.cpp
@@ -0,0 +1,16 @@
+// Egzamin.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Egzamin" << endl;
+	int x;
+	cin >> x;
+    return 0;
+}
+
